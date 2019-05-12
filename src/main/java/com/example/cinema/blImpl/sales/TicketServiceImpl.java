21ec5ab1796f39644d565eacad3516ac4c4e9daa
Heiming <280249027@qq.com>
2019-05-12 18:54:56 +0800

    feat:完成了TicketServiceImpl中的addTicket方法

--- src/main/java/com/example/cinema/blImpl/sales/TicketServiceImpl.java
+++ src/main/java/com/example/cinema/blImpl/sales/TicketServiceImpl.java
@@ -1,17 +1,18 @@
 package com.example.cinema.blImpl.sales;
 
+import com.example.cinema.bl.promotion.ActivityService;
+import com.example.cinema.bl.promotion.CouponService;
 import com.example.cinema.bl.sales.TicketService;
 import com.example.cinema.blImpl.management.hall.HallServiceForBl;
 import com.example.cinema.blImpl.management.schedule.ScheduleServiceForBl;
 import com.example.cinema.data.sales.TicketMapper;
-import com.example.cinema.po.Hall;
-import com.example.cinema.po.ScheduleItem;
-import com.example.cinema.po.Ticket;
+import com.example.cinema.po.*;
 import com.example.cinema.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,11 +27,32 @@ public class TicketServiceImpl implements TicketService {
     ScheduleServiceForBl scheduleService;
     @Autowired
     HallServiceForBl hallService;
+    @Autowired
+    CouponService couponService;
+    @Autowired
+    ActivityService activityService;
 
     @Override
     @Transactional
     public ResponseVO addTicket(TicketForm ticketForm) {
-        return null;
+        List<Ticket> tickets = new ArrayList<>();
+        List<SeatForm> seats = ticketForm.getSeats();
+        for (SeatForm seat: seats){
+            Ticket ticket = new Ticket();
+            ticket.setUserId(ticketForm.getUserId());
+            ticket.setScheduleId(ticketForm.getScheduleId());
+            ticket.setRowIndex(seat.getRowIndex());
+            ticket.setColumnIndex(seat.getColumnIndex());
+            ticket.setState(0);
+            tickets.add(ticket);
+        }
+        try {
+            ticketMapper.insertTickets(tickets);
+            return getOtherInfo(ticketForm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
     }
 
     @Override
@@ -77,5 +99,21 @@ public class TicketServiceImpl implements TicketService {
     }
 
 
-
+    private ResponseVO getOtherInfo(TicketForm ticketForm) {
+        try {
+            TicketWithCouponVO ticketWithCouponVO = new TicketWithCouponVO();
+            List<TicketVO> ticketVOList = new ArrayList<>();
+            for (SeatForm seat: ticketForm.getSeats()){
+                ticketVOList.add(ticketMapper.selectTicketByScheduleIdAndSeat(ticketForm.getScheduleId(), seat.getColumnIndex(), seat.getRowIndex()).getVO());
+            }
+            ticketWithCouponVO.setTicketVOList(ticketVOList);
+            ticketWithCouponVO.setTotal(ticketVOList.size() * scheduleService.getScheduleItemById(ticketForm.getScheduleId()).getFare());
+            ticketWithCouponVO.setCoupons((List<Coupon>) couponService.getCouponsByUser(ticketForm.getUserId()).getContent());
+            ticketWithCouponVO.setActivities((List<Activity>) activityService.getActivities().getContent());
+            return ResponseVO.buildSuccess(ticketWithCouponVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
 }
