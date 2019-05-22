1807faa905b8940b2d1bb1c6afdc645dc2347a46
steve_chph <876684433@qq.com>
2019-05-22 20:19:44 +0800

    [feature] 完成影厅管理的控制层和逻辑层，尚未单元测试

--- src/main/java/com/example/cinema/blImpl/management/hall/HallServiceImpl.java
+++ src/main/java/com/example/cinema/blImpl/management/hall/HallServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.cinema.blImpl.management.hall;
 
-import com.example.cinema.bl.management.HallService;
+import com.example.cinema.bl.management.hall.HallService;
 import com.example.cinema.data.management.HallMapper;
 import com.example.cinema.po.Hall;
 import com.example.cinema.vo.HallVO;
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author fjj
+ * @author fjj, chph
  * @date 2019/4/12 2:01 PM
  */
 @Service
@@ -28,6 +28,39 @@ public class HallServiceImpl implements HallService, HallServiceForBl {
             e.printStackTrace();
             return ResponseVO.buildFailure("失败");
         }
+    }
+
+    @Override
+    public ResponseVO addHall(HallVO hallVO) {
+        Hall hall = new Hall();
+        hall.setName(hallVO.getName());
+        hall.setScale(hallVO.getScale());
+        hall.setGeneratedSeats(hallVO.getSeats());
+        // result是返回插入结果的影响条数，为1时说明插入成功
+        int result = hallMapper.addHall(hall);
+        if (result == 1) return ResponseVO.buildSuccess();
+        else return ResponseVO.buildFailure("录入影厅信息失败！");
+    }
+
+    @Override
+    public ResponseVO updateHall(HallVO hallVO) {
+        Hall hall = new Hall();
+        hall.setId(hallVO.getId());
+        hall.setName(hallVO.getName());
+        hall.setScale(hallVO.getScale());
+        hall.setGeneratedSeats(hallVO.getSeats());
+        // result是返回更新结果的影响条数，为1时说明更新成功
+        int result = hallMapper.updateHall(hall);
+        if (result == 1) return ResponseVO.buildSuccess();
+        else return ResponseVO.buildFailure("更新影厅信息失败！");
+    }
+
+    @Override
+    public ResponseVO removeHall(int hallId) {
+        // result是返回删除结果的影响条数，为1时说明删除成功
+        int result = hallMapper.removeHallById(hallId);
+        if (result == 1) return ResponseVO.buildSuccess();
+        else return ResponseVO.buildFailure("删除影厅失败！");
     }
 
     @Override
