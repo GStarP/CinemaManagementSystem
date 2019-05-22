3b1b3cc45575f200dcfe0d20b6608fb8a9b131ed
steve_chph <876684433@qq.com>
2019-05-22 11:03:19 +0800

    [feature] 添加山寨版react框架（只支持渲染功能），实现了管理界面查看影厅的组件

---
+++ src/main/resources/static/js/adminCinemaManage/Component.js
@@ -0,0 +1,36 @@
+class Component {
+    constructor (props = {}) {
+        this.props = props
+    }
+    setState (state) {
+        const oldEl = this.el;
+        this.state = state
+        this.el = this.renderDOM()
+        if (this.onStateChange) this.onStateChange(oldEl, this.el)
+    }
+    renderDOM () {
+        this.el = createDOMFromString(this.render());
+        if (this.onClick) {
+            this.el.addEventListener('click', this.onClick.bind(this), false)
+        }
+        return this.el
+    }
+}
+
+const createDOMFromString = (domString) => {
+    const div = document.createElement('div')
+    div.innerHTML = domString
+    return div
+};
+
+const mount = (component, wrapper) => {
+    // wrapper.appendChild(component.renderDOM());
+    wrapper.append(component.renderDOM());
+    component.onStateChange = (oldEl, newEl) => {
+        // wrapper.insertBefore(newEl, oldEl);
+        wrapper.children().remove();
+        wrapper.append(newEl);
+        // wrapper.removeChild(oldEl)
+    }
+};
+
---
+++ src/main/resources/static/js/adminCinemaManage/ViewHall/ViewHall.js
@@ -0,0 +1,53 @@
+class ViewHall extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            halls: []
+        };
+        this.getCinemaHalls();
+    }
+
+    setState(state) {
+        super.setState(state);
+    }
+
+    getCinemaHalls() {
+        getRequest(
+            '/hall/all',
+            (res) => this.setState({halls: res.content}),
+            error => alert(JSON.stringify(error))
+        );
+    }
+
+    renderDOM() {
+        $('#hall-card').empty();
+        let hallDomStr = "";
+        this.state.halls.forEach(hall => {
+            const {id, name, seats, scale} = hall;
+            let seat = "";
+            // 座位布局
+            for (let i = 0; i < seats.length; i++) {
+                let temp = "";
+                for (let j = 0; j < seats[0].length; j++) {
+                    if (seats[i][j] === 0) temp += "<div class='cinema-hall-seat'></div>";
+                    else temp += "<div class='cinema-hall-seat-none'></div>";
+                }
+                seat+= "<div>"+temp+"</div>";
+            }
+
+            const hallDom =
+                "<div class='cinema-hall'>" +
+                "<div>" +
+                "<span class='cinema-hall-name'>" + name + "</span>" +
+                "<span class='cinema-hall-size'>" + seats[0].length + '*' + seats.length + "</span>" +
+                "</div>" +
+                "<div class='cinema-seat'>" + seat +
+                "</div>" +
+                "</div>";
+
+            hallDomStr += hallDom;
+        });
+
+        return hallDomStr;
+    }
+}
