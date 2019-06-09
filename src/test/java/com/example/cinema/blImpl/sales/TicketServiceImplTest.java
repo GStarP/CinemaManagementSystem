c1cae7bcde9b9d4d2ea9c10e94090ce7f9117abe
ccccz <171250013@smail.nju.edu.cn>
2019-06-09 18:09:41 +0800

    [feat] 添加文档图片

---
+++ src/test/java/com/example/cinema/blImpl/sales/TicketServiceImplTest.java
@@ -0,0 +1,56 @@
+package com.example.cinema.blImpl.sales;
+
+import com.example.cinema.CinemaApplication;
+import com.example.cinema.bl.sales.TicketService;
+import com.example.cinema.vo.ResponseVO;
+import com.example.cinema.vo.SeatForm;
+import com.example.cinema.vo.TicketForm;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(classes = CinemaApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TicketServiceImplTest {
+
+    @Autowired
+    TicketService ticketService;
+
+    @Test
+    public void addTicketTest(){
+        TicketForm ticketForm=new TicketForm();
+        SeatForm seatForm=new SeatForm();
+        List<SeatForm> seatForms=new ArrayList<>();
+        seatForm.setColumnIndex(0);
+        seatForm.setRowIndex(0);
+        ticketForm.setUserId(0);
+        ticketForm.setScheduleId(0);
+        seatForms.add(seatForm);
+        ticketForm.setSeats(seatForms);
+        ResponseVO responseVO=ticketService.addTicket(ticketForm);
+        Assert.assertEquals(true,responseVO.getSuccess());
+    }
+
+    @Test
+    public void completeTicketTest(){
+        List<Integer> integers=new ArrayList<>();
+        integers.add(0);
+        ResponseVO responseVO=ticketService.completeTicket(integers,0);
+        Assert.assertEquals(true,responseVO.getSuccess());
+    }
+
+    @Test
+    public void getByScheduleTest(){
+        ResponseVO responseVO=ticketService.getBySchedule(0);
+        Assert.assertEquals(true,responseVO.getSuccess());
+    }
+}
