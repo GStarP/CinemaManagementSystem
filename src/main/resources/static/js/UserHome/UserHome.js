3446e32214f6df1cee3d7fe9249a4a41579cf6bc
GStarP <171250605@smail.nju.edu.cn>
2019-06-10 20:16:46 +0800

    [refactor] 重构Banner组件

--- src/main/resources/static/js/UserHome/UserHome.js
+++ src/main/resources/static/js/UserHome/UserHome.js
@@ -56,13 +56,13 @@ $(document).ready(function () {
     mount(new MovieRank({hotMovies: movies}), document.querySelector("#user-home-ranking"));
     const images = [{
         url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1246180815,1068531272&fm=26&gp=0.jpg',
-        navigate: '#'
+        navi: '#'
     }, {
         url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559894859551&di=4c6309c77d87473a15c2c0b2da4f6aa7&imgtype=0&src=http%3A%2F%2Fimg4.a0bi.com%2Fupload%2Fttq%2F20180131%2F1517407295942.jpg',
-        navigate: '#'
+        navi: '#'
     }, {
         url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560489614&di=c0d417ec78d087bec02e61b7ec7f7bcb&imgtype=jpg&er=1&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F0a728a07a1dd3bca30ecd9c6e8f84b2573d52699.jpg',
-        navigate: '#'
+        navi: '#'
     }];
     mount(new Banner({images: images}), document.querySelector("#user-home-banner"));
 });
