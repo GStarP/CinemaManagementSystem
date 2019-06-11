a32c564c6a1ef35e92dc07a548b27d99d084cbf5
ccccz <171250013@smail.nju.edu.cn>
2019-06-11 19:20:48 +0800

    [smailfix] 修改了测试方法，使之可以使用

--- src/test/java/com/example/cinema/blImpl/user/RoleServiceImplTest.java
+++ src/test/java/com/example/cinema/blImpl/user/RoleServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.example.cinema.blImpl;
+package com.example.cinema.blImpl.user;
 
 import com.example.cinema.CinemaApplication;
 import com.example.cinema.bl.user.RoleService;
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+
 @SpringBootTest(classes = CinemaApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RoleServiceImplTest {
@@ -19,7 +21,7 @@ public class RoleServiceImplTest {
 
     @Test
     public void getAdaptableRolesTest(){
-        ResponseVO responseVO=roleService.getAdaptableRoles(0);
-        Assert.assertEquals(0,responseVO.getContent());
+        ResponseVO responseVO=roleService.getAdaptableRoles(2);
+        Assert.assertEquals(2,((ArrayList)responseVO.getContent()).size());
     }
 }
