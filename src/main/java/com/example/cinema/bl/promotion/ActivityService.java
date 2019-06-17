417a2f2dbd740179d878c792772f34123a5dba70
ccccz <171250013@smail.nju.edu.cn>
2019-06-17 20:27:10 +0800

    [style] 重构了activity的代码

--- src/main/java/com/example/cinema/bl/promotion/ActivityService.java
+++ src/main/java/com/example/cinema/bl/promotion/ActivityService.java
@@ -7,15 +7,39 @@ import com.example.cinema.vo.ResponseVO;
  * Created by liying on 2019/4/20.
  */
 public interface ActivityService {
-    
+
+    /**
+     * 新增一个活动
+     * @param activityForm
+     * @return
+     */
     ResponseVO publishActivity(ActivityForm activityForm);
 
+    /**
+     * 获取全部活动
+     * @return
+     */
     ResponseVO getActivities();
 
+    /**
+     * 删除活动
+     * @param activityId
+     * @return
+     */
     ResponseVO deleteActivity(int activityId);
 
+    /**
+     * 根据id获取活动
+     * @param activityId
+     * @return
+     */
     ResponseVO getActivityById(int activityId);
 
+    /**
+     * 根据电影id获取此电影的全部活动
+     * @param movieId
+     * @return
+     */
     ResponseVO getActivitiesByMovie(int movieId);
 
 }
--- src/main/java/com/example/cinema/data/promotion/ActivityMapper.java
+++ src/main/java/com/example/cinema/data/promotion/ActivityMapper.java
@@ -12,20 +12,57 @@ import java.util.List;
 @Mapper
 public interface ActivityMapper {
 
+    /**
+     * 新增一条活动
+     * @param activity
+     * @return
+     */
     int insertActivity(Activity activity);
 
+    /**
+     * 新增多个活动，活动相同电影不同
+     * @param activityId
+     * @param movieId
+     * @return
+     */
     int insertActivityAndMovie(@Param("activityId") int activityId,@Param("movieId") List<Integer> movieId);
 
+    /**
+     * 获取所有活动
+     * @return
+     */
     List<Activity> selectActivities();
 
+    /**
+     * 根据电影id获取此电影的所有活动
+     * @param movieId
+     * @return
+     */
     List<Activity> selectActivitiesByMovie(int movieId);
 
+    /**
+     * 根据id获取活动
+     * @param id
+     * @return
+     */
     Activity selectById(int id);
 
+    /**
+     * 获取没有电影的活动
+     * @return
+     */
     List<Activity> selectActivitiesWithoutMovie();
 
+    /**
+     * 根据id删除活动
+     * @param id
+     */
     void deleteActivityById(int id);
 
+    /**
+     * 根据活动id删除此活动关联电影信息
+     * @param id
+     */
     void deleteActivityAndMovie(int id);
 
 
